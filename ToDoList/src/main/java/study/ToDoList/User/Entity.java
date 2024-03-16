b3ef6797749565d65103c7c5188b20192158534f
Duna Seo <tjensk26@gmail.com>
2024-03-16 20:55:35 +0900

    add ToDoList

---
+++ ToDoList/src/main/java/study/ToDoList/User/Entity.java
@@ -0,0 +1,20 @@
+package study.ToDoList.User;
+
+import java.time.LocalDate;
+
+public class Entity {
+
+    private int userid;
+    private String name;
+
+    private enum gender;
+
+    private LocalDate birth;
+
+    private enum hometown;
+
+    private String login_id;
+
+    private String password;
+
+}
---
+++ ToDoList/src/main/java/study/ToDoList/User/Service.java
@@ -0,0 +1,4 @@
+package study.ToDoList.User;
+
+public class Service {
+}
