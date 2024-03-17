60bfce8eab0f2a4a6093e56424cfeb477f789e93
ensk26 <tjensk26@gmail.com>
2024-03-18 02:39:37 +0900

    feat: valid 추가

--- ToDoList/src/main/java/study/ToDoList/user/UserParameter.java
+++ ToDoList/src/main/java/study/ToDoList/user/UserParameter.java
@@ -1,14 +1,20 @@
 package study.ToDoList.user;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @NoArgsConstructor
 @Getter
 public class UserParameter {
 
+    @NotEmpty
+    @Length(max=255)
     private String login_id;
 
+    @NotEmpty
+    @Length(max=255)
     private String password;
 
 
--- ToDoList/src/main/java/study/ToDoList/user/UserRequest.java
+++ ToDoList/src/main/java/study/ToDoList/user/UserRequest.java
@@ -1,7 +1,14 @@
 package study.ToDoList.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -9,17 +16,27 @@ import java.time.LocalDate;
 @Getter
 public class UserRequest {
 
+    @NotEmpty
+    @Length(max=255)
     private String name;
 
+    @NotNull
     private Gender gender;
 
+    @NotNull
+    @DateTimeFormat(pattern = "yyyy-MM")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM", timezone = "Asia/Seoul")
     private LocalDate birth;
 
+    @NotNull
     private Hometown hometown;
 
-
+    @NotEmpty
+    @Length(max=255)
     private String login_id;
 
+    @NotEmpty
+    @Length(max=255)
     private String password;
 
     User to() {
