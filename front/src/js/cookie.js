0644e2560a90bb93e26a9495bf424de1b5d44ede
ensk26 <tjensk26@gmail.com>
2024-03-23 21:55:51 +0900

    feat: 쿠키를 이용한 자동 로그인 구현

---
+++ front/src/js/cookie.js
@@ -0,0 +1,34 @@
+function setCookie(ckey, cvalue){
+    const d = new Date();
+    d.setTime(d.getTime+(60*60*1000));
+    let expires ="expires="+d.toUTCString();
+    document.cookie = ckey +"="+cvalue+";"+expires+";path=/";
+}
+
+function getCookie(ckey){
+    let key = ckey+"=";
+    let ca = document.cookie.split(';');
+    for(let i =0;i<ca.length;i++){
+        let c =ca[i];
+        while(c.charAt(0)==' '){
+            c=c.substring(1);
+        }
+        if(c.indexOf(key)==0){
+            return c.substring(key.length, c.length);
+        }
+        return "";
+    }
+}
+
+function checkCookie(){
+    let token = getCookie("loginToken");
+    if(token !=""){
+        let html=`<p>${token}</p>`
+        document.getElementById("loginToken").innerHTML=html;
+
+    }else{
+        window.location.href="signin.html";
+    }
+}
+
+// export { setCookie }
--- front/src/js/signin.js
+++ front/src/js/signin.js
@@ -1,3 +1,5 @@
+// import { setCookie } from "./cookie.js"
+
 let singin_btn = document.getElementById("signin_btn");
 
 let username_input = document.getElementById("username");
@@ -9,13 +11,26 @@ let password;
 password_input.addEventListener("change", () => password = password_input.value);
 
 singin_btn.onclick = function () {
-    preventDefault()
+    // preventDefault()
     console.log("로그인 페이지로 이동합니다")
     console.log("username: ", username);
     console.log("password", password);
+
+    const token = "eyJpc0xvZ2luIjp0cnVlLCJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.sLC9uaPTSFGzcoWSNe91b505maoXtnHzhPI3-WbuWnc";
+    setCookie("loginToken", token);
+    window.location.href = "todo.html";
 };
 
 let singup_btn = document.getElementById("signup_btn");
 singup_btn.onclick = function () {
     console.log("회원가입 페이지로 이동합니다")
+    window.location.href = "signup.html";
 }
+
+function setCookie(ckey, cvalue){
+    const d = new Date();
+    d.setTime(d.getTime+(60*60*1000));
+    let expires ="expires="+d.toUTCString();
+    document.cookie = ckey +"="+cvalue+";"+expires+";path=/";
+}
+
