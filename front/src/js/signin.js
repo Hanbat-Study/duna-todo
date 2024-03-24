1bd20a1bc5d5a3112082e95b783c74d7bef390e0
ensk26 <tjensk26@gmail.com>
2024-03-24 11:44:55 +0900

    feat: 로그인 유효성 검사 기능 추가

--- front/src/js/signin.js
+++ front/src/js/signin.js
@@ -2,19 +2,25 @@
 
 let singin_btn = document.getElementById("signin_btn");
 
-let username_input = document.getElementById("username");
-let username;
-username_input.addEventListener("change", () => username = username_input.value);
+let id_input = document.getElementById("id");
+let id;
+id_input.addEventListener("change", function () {
+    id = id_input.value;
+    emailValid(id);
+    disabled_btn();
+});
 
 let password_input = document.getElementById("password");
 let password;
-password_input.addEventListener("change", () => password = password_input.value);
+password_input.addEventListener("change", function () {
+    password = password_input.value
+    passwordValid(password);
+    disabled_btn();
+});
 
 singin_btn.onclick = function () {
-    // preventDefault()
     console.log("로그인 페이지로 이동합니다")
-    console.log("username: ", username);
-    console.log("password", password);
+
 
     const token = "eyJpc0xvZ2luIjp0cnVlLCJ0eXBlIjoiSldUIiwiYWxnIjoiSFMyNTYifQ.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.sLC9uaPTSFGzcoWSNe91b505maoXtnHzhPI3-WbuWnc";
     setCookie("loginToken", token);
@@ -27,10 +33,57 @@ singup_btn.onclick = function () {
     window.location.href = "signup.html";
 }
 
-function setCookie(ckey, cvalue){
+function setCookie(ckey, cvalue) {
     const d = new Date();
-    d.setTime(d.getTime+(60*60*1000));
-    let expires ="expires="+d.toUTCString();
-    document.cookie = ckey +"="+cvalue+";"+expires+";path=/";
+    d.setTime(d.getTime + (60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = ckey + "=" + cvalue + ";" + expires + ";path=/";
 }
 
+
+const reEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+const resPass = /^[A-Za-z0-9]{8,20}$/;
+
+const res = /^[A-Za-z0-9]{1,20}$/;
+
+let ppsw = document.getElementById("ppsw");
+let pid = document.getElementById("pid");
+
+let fpsw = false;
+let fid = false;
+
+function emailValid(id) {
+    if (!reEmail.exec(id)) {
+        pid.innerHTML = "@”, “.” 포함";
+        fid = false;
+    } else {
+        pid.innerHTML = "만족";
+        fid = true;
+    }
+    console.log(fid);
+}
+
+function passwordValid(password) {
+    if (!resPass.exec(password)) {
+        ppsw.innerHTML = "8자 이상";
+        fpsw = false;
+    } else {
+        ppsw.innerHTML = "만족";
+        fpsw = true;
+    }
+    console.log(fpsw);
+}
+
+
+singin_btn.disabled = true;
+
+
+function disabled_btn() {
+    if (fpsw && fid) {
+        singin_btn.disabled = false;
+    }
+    else {
+        singin_btn.disabled = true;
+    }
+}
